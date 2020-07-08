cd5e9e9f0d24d057cfaddf2ab477c985081cea83
yki <yki@192.168.200.149>
2020-07-09 00:10:05 +0900

    JPA 관련 Commit

---
+++ src/main/java/com/lgcns/swainno/jpa/web/sample4/domain/Sample4Repository.java
@@ -0,0 +1,7 @@
+package com.lgcns.swainno.jpa.web.sample4.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Sample4Repository extends JpaRepository<Member, Long>{
+
+}
