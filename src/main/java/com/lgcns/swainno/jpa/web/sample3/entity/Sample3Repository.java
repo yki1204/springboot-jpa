4fd1c8d4c4e23111e287127c0e658720780a3b72
YeonjeeRyu <67727605+YeonjeeRyu@users.noreply.github.com>
2020-07-10 15:12:05 +0900

    Delete Sample3Repository.java

--- src/main/java/com/lgcns/swainno/jpa/web/sample3/entity/Sample3Repository.java
+++
@@ -1,7 +0,0 @@
-package com.lgcns.swainno.jpa.web.sample3.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Sample3Repository extends JpaRepository<MemberE, Long>{
-
-}
