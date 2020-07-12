f611b4f026f2dadc1637f8573a5f1cb873b4cf3f
yki <yki@192.168.200.149>
2020-07-12 16:22:21 +0900

    .

--- src/main/java/com/lgcns/swainno/jpa/web/sample4/MemberDTO.java
+++ src/main/java/com/lgcns/swainno/jpa/web/sample4/MemberDTO.java
@@ -1,5 +1,8 @@
 package com.lgcns.swainno.jpa.web.sample4;
 
+import lombok.Data;
+
+@Data
 public class MemberDTO {
 	
 	private long id;
@@ -9,37 +12,5 @@ public class MemberDTO {
 	private int age;
 	
 	private String CompanyName;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return Name;
-	}
-
-	public void setName(String name) {
-		Name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public String getCompanyName() {
-		return CompanyName;
-	}
-
-	public void setCompanyName(String companyName) {
-		CompanyName = companyName;
-	}
 	
 }
