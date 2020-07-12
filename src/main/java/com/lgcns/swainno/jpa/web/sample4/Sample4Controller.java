42ad4963d1c709fd2a4baa5b88e8c365e461b89e
yki <yki@192.168.200.149>
2020-07-12 16:00:32 +0900

    .

--- src/main/java/com/lgcns/swainno/jpa/web/sample4/Sample4Controller.java
+++ src/main/java/com/lgcns/swainno/jpa/web/sample4/Sample4Controller.java
@@ -1,10 +1,26 @@
 package com.lgcns.swainno.jpa.web.sample4;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
 /**
  * 
  * @author yki
  *
  */
+@RestController
+@AllArgsConstructor
 public class Sample4Controller {
+	
+	private Sample4Service sample4Service;
+
+	@GetMapping("/sample4/members")
+	public List<MemberDTO> retrieveMemberList(){
+		return sample4Service.retrieveMemberList();
+	}
 
 }
