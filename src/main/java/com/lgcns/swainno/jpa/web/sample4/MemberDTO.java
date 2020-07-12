cfeb00794793bebbd365b1ccbd540abb6d2eff25
yki <yki@192.168.200.149>
2020-07-12 16:00:41 +0900

    .

---
+++ src/main/java/com/lgcns/swainno/jpa/web/sample4/MemberDTO.java
@@ -0,0 +1,45 @@
+package com.lgcns.swainno.jpa.web.sample4;
+
+public class MemberDTO {
+	
+	private long id;
+	
+	private String Name;
+	
+	private int age;
+	
+	private String CompanyName;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCompanyName() {
+		return CompanyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		CompanyName = companyName;
+	}
+	
+}
